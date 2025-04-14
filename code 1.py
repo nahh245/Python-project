c28c435a1c2b396371b2f09929b35cf794fbfdae
nahh245 <nahomjohn245@gmail.com>
2025-04-15 02:14:39 +0300

    Add files via upload

---
+++ code 1.py
@@ -0,0 +1,15 @@
+#code 1
+
+original_string = "Hello World!"
+uppercased_string = original_string.lower()
+print(uppercased_string) 
+
+
+original_string = "Hello World!"
+uppercased_string = original_string.upper()
+print(uppercased_string)  # Output: HELLO WORLD!
+
+
+original_string = "hello world! welcome to python programming."
+titled_string = original_string.title()
+print(titled_string)  # Output: Hello World! Welcome To Python Programming.
