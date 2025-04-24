73390e5e91987f8deecc07c2d2fb7777d6d76b38
nahh245 <nahomjohn245@gmail.com>
2025-04-25 01:21:15 +0300

    Add files via upload

---
+++ code 9.py
@@ -0,0 +1,25 @@
+#code 9
+
+lowercase_string = "hello world"
+print(lowercase_string.islower())  # Output: True
+
+mixed_case_string = "Hello World"
+print(mixed_case_string.islower())  # Output: False
+
+empty_string = ""
+print(empty_string.islower())  # Output: False
+
+string_with_numbers = "abc123"
+print(string_with_numbers.islower())  # Output: True
+
+uppercase_string = "HELLO WORLD"
+print(uppercase_string.isupper())  # Output: True
+
+mixed_case_string = "Hello World"
+print(mixed_case_string.isupper())  # Output: False
+
+empty_string = ""
+print(empty_string.isupper())  # Output: False
+
+string_with_numbers = "ABC123"
+print(string_with_numbers.isupper())  # Output: True
