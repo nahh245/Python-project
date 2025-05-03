56ba1a91ced1c414fe702782b566b042fd9a1d61
nahh245 <nahomjohn245@gmail.com>
2025-05-03 22:30:59 +0300

    Add files via upload

---
+++ Listing methods/code 4.py
@@ -0,0 +1,13 @@
+# creating listing method
+# 10. reverse
+
+people = ['bush', 'obama', 'trump']
+people.reverse()
+print(people)#output: ['trump', 'obama', 'bush']
+
+# 11.sort
+
+people = ['TRUMP', 'VANCE', 'MIKE', 'MARCO']
+people.sort()
+
+print(people)# output: ['MARCO', 'MIKE', 'TRUMP', 'VANCE']
